047cc88f6974943e6fff98707ae089beb3224168
jp86 <413670706@qq.com>
2016-12-23 22:04:40 +0800

    邮件发送乱码处理及新密码修改 --jp

--- ccs/src/main/java/com/yc/ccs/web/handler/UserHandler.java
+++ ccs/src/main/java/com/yc/ccs/web/handler/UserHandler.java
@@ -1,7 +1,10 @@
 package com.yc.ccs.web.handler;
 
+import java.util.Random;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -19,16 +22,16 @@ public class UserHandler {
 	private JavaMailSender mailSender;
 
 	@RequestMapping("/forget")
-	public String forget(String username, String email, HttpSession session) {
+	public String forget(String username, String email, HttpServletRequest request) {
 		LogManager.getLogger().debug("请求UserHandler进行forget的操作....");
 		LogManager.getLogger().debug("请求数据username:" + username + ", email:" + email);
 		try {
 			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
 			helper.setFrom("studymail_test@163.com");
 			helper.setTo(email);
 			helper.setSubject("找回密码");
-			String hrefStr = session.getServletContext().getContextPath() + "/user/getpassword?username=" + username;
+			String hrefStr = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getServletContext().getContextPath() + "/user/getpassword?username=" + username;
 			System.out.println(hrefStr);
 			helper.setText("<a href='" + hrefStr + "'>找回密码</a><br>如果连接不可用拷贝" + hrefStr + "到地址栏...", true);
 			mailSender.send(message);
@@ -38,5 +41,14 @@ public class UserHandler {
 		}
 
 		return "redirect:/page/forgetSuccess.jsp";
+	}
+	
+	@RequestMapping("/getpassword")
+	public String getpassword(String username, HttpSession session) {
+		Random rand = new Random();
+		String randPassword = rand.nextInt(900000) + 100000 + "";
+		//业务处理重置密码
+		session.setAttribute("newPassword", randPassword);
+		return "redirect:/page/getpasswordSuccess.jsp";
 	}
 }
